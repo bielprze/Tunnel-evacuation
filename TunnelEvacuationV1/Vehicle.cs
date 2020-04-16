28a963133826611a56ad373e1a3f661bc229a9b5
Przemysław Bielecki <33664570+bielprze@users.noreply.github.com>
2020-04-16 17:05:42 +0200

    Add files via upload

---
+++ TunnelEvacuationV1/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TunnelEvacuationV1
+{
+    public enum vehicle_types
+    {
+        Truck = 0,
+        Car = 1,
+        Bike = 2
+    }
+    class Vehicle
+    {
+        vehicle_types vehicle_type;
+        int x;
+        int y;
+        int passenger;
+        int passengers_reaction_time;
+
+        public Vehicle()
+        {
+
+        }
+        void fill_passangers()
+        {
+
+        }
+
+        void reaction_time()
+        {
+
+        }
+
+        void location(int a, int b)
+        {
+            this.x = a;
+            this.y = b;
+        }
+    }
+}
