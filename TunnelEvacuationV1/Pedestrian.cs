2b7e8a577df2fb4d3e915d7cf68c7eb2be7f21cd
Przemysław Bielecki <33664570+bielprze@users.noreply.github.com>
2020-04-16 17:44:58 +0200

    Add files via upload

---
+++ TunnelEvacuationV1/Pedestrian.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TunnelEvacuationV1
+{
+    class Pedestrian
+    {
+        int panic;
+        double speed;
+        public Pedestrian(int p, double s)
+        {
+            panic = p;
+            speed = s;
+        }
+
+    }
+}
--- TunnelEvacuationV1/Vehicle.cs
+++ TunnelEvacuationV1/Vehicle.cs
@@ -20,18 +20,51 @@ namespace TunnelEvacuationV1
         int passenger;
         int passengers_reaction_time;
 
-        public Vehicle()
+        public Vehicle(int en, int a, int b)
         {
-
+            vehicle_type = (vehicle_types)en;
+            fill_passangers();
+            reaction_time();
+            location(a, b);
         }
         void fill_passangers()
         {
+            var rand = new Random();
+            int i = (int)vehicle_type;
 
+            switch (i)
+            {
+                case 0:
+                    if (rand.NextDouble() < 0.7)
+                        passenger = 1;
+                    else
+                        passenger = 2;
+                    break;
+                case 1:
+                    if (rand.NextDouble() < 0.26)
+                        passenger = 1;
+                    else if(rand.NextDouble() < 0.51)
+                        passenger = 2;
+                    else if (rand.NextDouble() < 0.76)
+                        passenger = 3;
+                    else if (rand.NextDouble() < 0.91)
+                        passenger = 4;
+                    else 
+                        passenger = 5;
+                    break;
+                case 2:
+                    if (rand.NextDouble() < 0.9)
+                        passenger = 1;
+                    else
+                        passenger = 2;
+                    break;
+            }
         }
 
         void reaction_time()
         {
-
+            var rand = new Random();
+            passengers_reaction_time = rand.Next(30, 120);
         }
 
         void location(int a, int b)
