39e8350eb52f110f20419494a6c571766acd5980
Przemysław Bielecki <33664570+bielprze@users.noreply.github.com>
2020-04-16 16:55:30 +0200

    Add files via upload

---
+++ TunnelEvacuationV1/DataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TunnelEvacuationV1
+{
+    public static class DataBase
+    {
+        public static int evac_time;
+        public static int vehicle_num;
+        public static int interval;
+
+        public static int tir;
+        public static int car;
+        public static int bike;
+
+        public static double tir_percent = 0.5;
+        public static double car_percent = 0.4;
+        public static double bike_percent = 0.1;
+
+        public static void evaluate_vehicles()
+        {
+            tir = (int)tir_percent * vehicle_num;
+            car = (int)car_percent * vehicle_num;
+            bike = (int)bike_percent * vehicle_num;
+        }
+    }
+}
